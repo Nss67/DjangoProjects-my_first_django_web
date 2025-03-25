e2654c37b50ccdd7a54e2cc26bc7d0d387d25a45
Nss67 <milade67@hotmail.com>
2025-03-26 00:50:38 +0330

    created comment model and istall ipython

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+
+class CommentAdminInLine(admin.StackedInline):
+    model = Comment
+    fields = ["post", "text"]
+    extra = 0
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "is_enable", "publish_date", "created_time"]
+    inlines = [CommentAdminInLine,]
+
+
+
+# class CommentAdmin(admin.ModelAdmin):
+    # list_display = ["id", "post", "created_time", "text"]
+
+
+# admin.site.register(Post, PostAdmin)
+# admin.site.register(Comment, CommentAdmin)
