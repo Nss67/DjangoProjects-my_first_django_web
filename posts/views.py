90cc4ca99c6cdc6c633fa905577f21126e508469
Nss67 <milade67@hotmail.com>
2025-03-30 07:23:03 +0330

    finished

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
+from django.views import generic
+
 from .models import Post, Comment
 from .forms import PostForm
 
+
 def index(request):
     # body
-    return HttpResponse("<h1>Hello, World.</h1>")
+    return HttpResponse("<h1>Hello, World.</h1><br><h2>This my first web site.</h2>")
+
 
 def home(request):
     return HttpResponse("<h3>Welcome to my home page...</h3>")
+
 
 def post_list(request):
     posts = Post.objects.all()
     context = {"posts": posts}
     return render(request, "posts/post_list.html", context=context)
+
+
+class PostList(generic.ListView):
+    query_set = Post.objects.all()
+    template_name = "posts/post_list.html"
+    context_object_name = "posts"
+
 
 def post_detail(request, post_id):
     try:
@@ -24,6 +36,7 @@ def post_detail(request, post_id):
     comment = Comment.objects.filter(post=post)
     context = {"post": post, "comment": comment}
     return render(request, "posts/post_detail.html", context=context)
+
 
 def post_create(request):
     if request.method == "POST":
